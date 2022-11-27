a1e6d061506e4f69540c0e7371067caf4534a2e9
willianlim <willianrosendodelima@outlook.com>
2022-11-27 00:01:14 -0300

    first commit

---
+++ src/test/java/com/spring/springBootWebApp/SpringBootWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springBootWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
